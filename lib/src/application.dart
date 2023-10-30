b1688f4d8af4ab6ddbb1546f4e42fbc56b22ec83
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-30 14:25:40 +0600

    application: application routes implemented

--- lib/src/application.dart
+++ lib/src/application.dart
@@ -1,5 +1,7 @@
-import 'package:chuck_norris/src/modules/home/home_page.dart';
 import 'package:flutter/material.dart';
+
+import 'package:chuck_norris/src/core/config/config.dart';
+import 'package:chuck_norris/src/core/routes/app_pages.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
@@ -7,14 +9,18 @@ class Application extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter app",
+    final config = BuildConfig.instance.envConfig;
+    final env = BuildConfig.instance.environment;
+
+    return MaterialApp.router(
+      title: config.appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         // fontFamily: 'Montserrat',
       ),
-      home: const MyHomePage(title: "FLutter title"),
+      routerConfig: AppPages.routes,
+      debugShowCheckedModeBanner: env == Environment.DEVELOPMENT,
     );
   }
 }
