9eab711683d447dc1dae7381e9e2d4710b806814
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-30 14:24:04 +0600

    main: added build config and environment

--- lib/main/main_prod.dart
+++ lib/main/main_prod.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+import 'package:chuck_norris/src/core/config/config.dart';
 import 'package:chuck_norris/src/application.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final envConfig = EnvConfig(
+    appName: packageInfo.appName,
+    appVersion: packageInfo.version,
+  );
+
+  BuildConfig.instantiate(
+    config: envConfig,
+    type: Environment.PRODUCTION,
+  );
+
   runApp(
     const Application(),
   );
