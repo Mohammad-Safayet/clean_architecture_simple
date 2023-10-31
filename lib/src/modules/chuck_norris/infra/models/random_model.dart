932e3acbc9db04cf61cf3a9dc36203baa2b7b59b
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-10-31 19:56:31 +0600

    chuck norris module: random model implemented

---
+++ lib/src/modules/chuck_norris/infra/models/random_model.dart
@@ -0,0 +1,32 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'random_model.g.dart';
+
+@JsonSerializable()
+class RandomModel {
+  final List<String> categories;
+  @JsonKey(name: "created_at")
+  final String createdAt;
+  @JsonKey(name: "updated_at")
+  final String updatedAt;
+  @JsonKey(name: "icon_url")
+  final String iconUrl;
+  final String id;
+  final String url;
+  final String value;
+
+  RandomModel({
+    required this.categories,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.iconUrl,
+    required this.id,
+    required this.url,
+    required this.value,
+  });
+
+  factory RandomModel.fromJson(Map<String, dynamic> json) =>
+      _$RandomModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RandomModelToJson(this);
+}
