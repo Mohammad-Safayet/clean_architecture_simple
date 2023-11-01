9443740d6cb66c89096fdd0b4a1ca20bcfd0460d
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-01 08:20:36 +0600

    chuck norris module: implemnted jokes repo

--- lib/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart
+++ lib/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart
@@ -1,22 +1,36 @@
+import 'package:chuck_norris/src/core/extensions/models/chuck_norris/categories_model_extensions.dart';
+import 'package:chuck_norris/src/core/extensions/models/chuck_norris/random_model_extensions.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/infra/datasources/remote/jokes_remote_datasource.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/infra/repositories/jokes_repo.dart';
-import 'package:chuck_norris/src/modules/shared/base/base_remote_datasource.dart';
 
 class JokesRepositoryImpl extends JokesRepository {
-  final BaseRemoteDatasource jokesDatasource;
+  final JokesRemoteDatasource datasource;
 
   JokesRepositoryImpl({
-    required this.jokesDatasource,
+    required this.datasource,
   });
 
   @override
-  Future getJokesCategories() {
-    // TODO: implement getJokesCategories
-    throw UnimplementedError();
+  Future getJokesCategories() async {
+    try {
+      final model = await datasource.getJokesCategories();
+      final entity = model.toEntity();
+
+      return entity;
+    } catch(error) {
+      rethrow;
+    }
   }
 
   @override
-  Future getJokesRandom() {
-    // TODO: implement getJokesRandom
-    throw UnimplementedError();
+  Future getJokesRandom() async {
+    try {
+      final model = await datasource.getRandomJokes();
+      final entity = model.toEntity();
+
+      return entity;
+    } catch(error) {
+      rethrow;
+    }
   }
 }
