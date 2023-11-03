4996b66f9a4199661bfb6730f86352f0834c6272
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-03 06:45:44 +0600

    home module: added home view

---
+++ lib/src/modules/home/presentation/views/home.dart
@@ -0,0 +1,11 @@
+import 'package:chuck_norris/src/modules/chuck_norris/chuck_norris.dart';
+import 'package:chuck_norris/src/modules/shared/base/base_widget_mixin.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget with BaseWidgetMixin {
+  @override
+  Widget? body(BuildContext context) {
+    return const ChuckNorrisPage();
+  }
+
+}
