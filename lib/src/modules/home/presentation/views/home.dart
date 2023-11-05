767c45e2233bd3028e3544cea6899ae590a443b4
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-05 07:59:17 +0600

    dart fix --apply

--- lib/src/modules/home/presentation/views/home.dart
+++ lib/src/modules/home/presentation/views/home.dart
@@ -1,4 +1,4 @@
-import 'package:chuck_norris/src/modules/chuck_norris/chuck_norris.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/chuck_norris_page.dart';
 import 'package:chuck_norris/src/modules/shared/base/base_widget_mixin.dart';
 import 'package:flutter/material.dart';
 
@@ -7,5 +7,4 @@ class HomeView extends StatelessWidget with BaseWidgetMixin {
   Widget? body(BuildContext context) {
     return const ChuckNorrisPage();
   }
-
 }
