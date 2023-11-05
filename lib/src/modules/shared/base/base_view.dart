e62e3345e0c56f3a6b18500631ce4404ef9436ac
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-05 07:57:50 +0600

    shared module: fixed data reset bug

--- lib/src/modules/shared/base/base_view.dart
+++ lib/src/modules/shared/base/base_view.dart
@@ -1,28 +1,53 @@
 import 'package:chuck_norris/src/modules/shared/widgets/loading.dart';
 import 'package:flutter/material.dart';
 
-abstract class BaseView<T extends Object> extends StatelessWidget {
+abstract class BaseView<T extends Object> extends StatefulWidget {
+  Widget body(
+    BuildContext context,
+    T data,
+    VoidCallback setState,
+  );
+
+  Widget errorBody(
+    BuildContext context,
+    Object error,
+    VoidCallback setState,
+  );
+
+  Future<T> getData();
+
+  @override
+  State<BaseView<T>> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends Object> extends State<BaseView<T>> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<T>(
-      future: getData(),
+      future: widget.getData(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const LoadingWidget();
         } else {
-          if(snapshot.hasData) {
-            return body(context, snapshot);
+          if (snapshot.hasData) {
+            return widget.body(
+              context,
+              snapshot.data!,
+              () {
+                setState(() {});
+              },
+            );
           } else {
-            return errorBody(context, snapshot);
+            return widget.errorBody(
+              context,
+              snapshot.error!,
+              () {
+                setState(() {});
+              },
+            );
           }
         }
       },
     );
   }
-
-  Widget body(BuildContext context, AsyncSnapshot<T> snapshot);
-
-  Widget errorBody(BuildContext context, AsyncSnapshot<T> snapshot);
-
-  Future<T> getData();
 }
