24804cf93a3da134a23fafd025427666e2867deb
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 07:46:12 +0600

    chuck norries: chuck norries view added

--- lib/src/modules/chuck_norris/chuck_norris_page.dart
+++ lib/src/modules/chuck_norris/chuck_norris_page.dart
@@ -3,6 +3,7 @@ import 'package:chuck_norris/src/core/utils/di/di.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/infra/datasources/remote/jokes_remote_datasource_impl.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/categories.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/chuck_norries.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/joke.dart';
 import 'package:flutter/material.dart';
 
@@ -11,17 +12,7 @@ class ChuckNorrisPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: AppValues.margin,),
-      child: Column(
-        children: [
-          CategoriesView(),
-          Expanded(
-            child: JokesView(),
-          ),
-        ],
-      ),
-    );
+    return const ChuckNorrisView();
   }
 
   static void registerDependencies() {
