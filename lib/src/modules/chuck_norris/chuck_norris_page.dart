112a3a4e6948dd51dd7d8994f81a57697fcd6e8c
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 06:55:52 +0600

    chuck norris: refactored joke view

--- lib/src/modules/chuck_norris/chuck_norris_page.dart
+++ lib/src/modules/chuck_norris/chuck_norris_page.dart
@@ -3,7 +3,7 @@ import 'package:chuck_norris/src/core/utils/di/di.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/infra/datasources/remote/jokes_remote_datasource_impl.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/jokes.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/joke.dart';
 import 'package:flutter/material.dart';
 
 class ChuckNorrisPage extends StatelessWidget {
