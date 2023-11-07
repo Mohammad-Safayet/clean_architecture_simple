b42a1394a37d365ffa26be07b43a856ba8d64c75
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 06:47:18 +0600

    chuck norris: implemented error widget

---
+++ lib/src/modules/chuck_norris/presentation/widgets/error.dart
@@ -0,0 +1,46 @@
+import 'package:chuck_norris/src/core/constants/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:chuck_norris/src/core/utils/error_handling/error_handling.dart';
+import 'package:chuck_norris/src/modules/shared/base/base_widget_mixin.dart';
+
+class ErrorWidget extends StatelessWidget with BaseWidgetMixin {
+  ErrorWidget({
+    Key? key,
+    required this.data,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final FailureEntity data;
+  final Function onPressed;
+
+  @override
+  Widget body(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.error,
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            data.title,
+            style: AppTextStyles.errorTitle,
+          ),
+          Text(
+            data.message,
+            style: AppTextStyles.errorDesc,
+          ),
+          OutlinedButton(
+            onPressed: () {
+              onPressed();
+            },
+            child: const Text(
+              "Reload",
+              style: AppTextStyles.errorButton,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
