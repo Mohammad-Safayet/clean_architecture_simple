72f0af8cecdd38940d9ea3bf775ffa0b6d607bf7
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 06:53:07 +0600

    chuck norris: added error body widget to the errorview

--- lib/src/modules/chuck_norris/presentation/views/categories.dart
+++ lib/src/modules/chuck_norris/presentation/views/categories.dart
@@ -1,23 +1,13 @@
+import 'package:flutter/material.dart';
+
 import 'package:chuck_norris/src/core/utils/error_handling/error_handling.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/usecases/get_categories_usecase.dart';
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import 'package:chuck_norris/src/core/constants/app_text_styles.dart';
 import 'package:chuck_norris/src/core/constants/app_values.dart';
 import 'package:chuck_norris/src/core/utils/di/di.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/joke.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/usecases/get_random_jokes_usecase.dart';
 import 'package:chuck_norris/src/modules/shared/base/base_view.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/presentation/widgets/error.dart';
 
 class CategoriesView extends BaseView<Categories> {
   @override
@@ -64,33 +54,12 @@ class CategoriesView extends BaseView<Categories> {
       );
     }
 
-    return Container(
-      color: Theme.of(context).colorScheme.error,
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            data.title,
-            style: AppTextStyles.errorTitle,
-          ),
-          Text(
-            data.message,
-            style: AppTextStyles.errorDesc,
-          ),
-          OutlinedButton(
-            onPressed: () {
-              getData();
-              setState();
-            },
-            child: const Text(
-              "Reload",
-              style: AppTextStyles.errorButton,
-            ),
-          ),
-        ],
-      ),
+    return ErrorBodyWidget(
+      data: data,
+      onPressed: () {
+        getData();
+        setState();
+      },
     );
   }
 
