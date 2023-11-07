9d5e2d488afdea5534c8ae5443fbecf02ee3d844
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 07:59:21 +0600

    chuck norries: changed logic of the categories view

--- lib/src/modules/chuck_norris/presentation/views/chuck_norries.dart
+++ lib/src/modules/chuck_norris/presentation/views/chuck_norries.dart
@@ -1,10 +1,13 @@
 import 'package:chuck_norris/src/core/constants/app_values.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/categories.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/joke.dart';
 import 'package:flutter/material.dart';
 
 class ChuckNorrisView extends StatefulWidget {
-  const ChuckNorrisView({Key? key}) : super(key: key);
+  const ChuckNorrisView({Key? key, required this.categories,}) : super(key: key);
+
+  final Categories categories;
 
   @override
   State<ChuckNorrisView> createState() => _ChuckNorrisViewState();
@@ -28,6 +31,7 @@ class _ChuckNorrisViewState extends State<ChuckNorrisView> {
       child: Column(
         children: [
           CategoriesView(
+            data: widget.categories,
             activeItemName: category,
             voidCallback: setCategory,
           ),
