9b26f558cff216e5c580cefe87f3000080077298
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 07:58:54 +0600

    chuck norries: changed logic of the categories view

--- lib/src/modules/chuck_norris/presentation/views/categories.dart
+++ lib/src/modules/chuck_norris/presentation/views/categories.dart
@@ -1,30 +1,23 @@
-import 'package:chuck_norris/src/modules/shared/widgets/ripple_button.dart';
 import 'package:flutter/material.dart';
 
-import 'package:chuck_norris/src/core/utils/error_handling/error_handling.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/entities/categories.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/usecases/get_categories_usecase.dart';
+import 'package:chuck_norris/src/modules/shared/widgets/ripple_button.dart';
 import 'package:chuck_norris/src/core/constants/app_values.dart';
-import 'package:chuck_norris/src/core/utils/di/di.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart';
-import 'package:chuck_norris/src/modules/shared/base/base_view.dart';
-import 'package:chuck_norris/src/modules/chuck_norris/presentation/widgets/error.dart';
 
-class CategoriesView extends BaseView<Categories> {
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({
+    Key? key,
+    required this.data,
+    required this.voidCallback,
+    required this.activeItemName,
+  }) : super(key: key);
+
+  final Categories data;
   final Function voidCallback;
   final String activeItemName;
 
-  CategoriesView({
-    required this.voidCallback,
-    required this.activeItemName,
-  });
-
   @override
-  Widget body(
-    BuildContext context,
-    Categories data,
-    VoidCallback setState,
-  ) {
+  Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
       child: ListView.builder(
@@ -53,48 +46,5 @@ class CategoriesView extends BaseView<Categories> {
         },
       ),
     );
-  }
-
-  @override
-  Widget errorBody(
-    BuildContext context,
-    Object error,
-    VoidCallback setState,
-  ) {
-    final FailureEntity data;
-
-    if (error is FailureEntity) {
-      data = error;
-    } else {
-      data = ApplicationFailure(
-        message: error.toString(),
-        title: 'Unexpected error occurred',
-      );
-    }
-
-    return ErrorBodyWidget(
-      data: data,
-      onPressed: () {
-        getData();
-        setState();
-      },
-    );
-  }
-
-  @override
-  Future<Categories> getData() async {
-    final di = DependencyInjection.instance;
-    final categoriesUsecase =
-        GetCategoriesUsecase(repository: di.getIt<JokesRepositoryImpl>());
-
-    final joke = await categoriesUsecase.call();
-
-    final result = joke.fold((l) => l, (r) => r);
-
-    if (result is Categories) {
-      return result;
-    } else {
-      throw result as FailureEntity;
-    }
   }
 }
