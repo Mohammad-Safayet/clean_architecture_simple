e345266971fa674f5cfa2b2a73f116ecb9a4f46b
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-11-07 06:39:19 +0600

    chuck norris: added category view in chuck_norris_page

--- lib/src/modules/chuck_norris/chuck_norris_page.dart
+++ lib/src/modules/chuck_norris/chuck_norris_page.dart
@@ -2,6 +2,7 @@ import 'package:chuck_norris/src/core/constants/app_values.dart';
 import 'package:chuck_norris/src/core/utils/di/di.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/domain/repositories/jokes_repo_impl.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/infra/datasources/remote/jokes_remote_datasource_impl.dart';
+import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/categories.dart';
 import 'package:chuck_norris/src/modules/chuck_norris/presentation/views/jokes.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,15 @@ class ChuckNorrisPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(AppValues.padding),
-      child: JokesView(),
+      margin: const EdgeInsets.only(top: AppValues.margin,),
+      child: Column(
+        children: [
+          CategoriesView(),
+          Expanded(
+            child: JokesView(),
+          ),
+        ],
+      ),
     );
   }
 
